6a33790cb8f82515d7586c8acfbc5036e708bf70
Dan Laugharn <laugharn@gmail.com>
2021-05-22 10:07:34 -0500

    Implements Prisma

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 A simple social bookmarking proof of concept built on a rainy Saturday morning.
 
+## Prerequisites
+
+* Yarn
+* Node 14.x or higher
+* Postgresql 13.x
+
 ## Up and Running
 
 * Run `yarn` to install dependencies
+* Copy .env.local.example to .env.local and update the value for `DATABASE_URL`
+* Run `yarn db:init` to get the Prisma environment configured (it doesn't recognize the Next/CRA named environment files yet) and reset the database to the current schema.
 * Run `yarn dev` to run the application at localhost:3000
 
 ## TODO
 
 - [x] Initial setup
-- [ ] Implement Prisma
+- [x] Implement Prisma
 - [ ] Link API
 - [ ] Link create UI
 - [ ] Filtering UI
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-const Page = () => {
+import { PrismaClient } from '@prisma/client'
+
+const Page = ({ posts }) => {
   return (
     <div className="leading-6 md:leading-10 p-2 text-lg md:text-4xl w-full">
-      A simple social bookmarking proof of concept built on a rainy Saturday morning.
+      A simple social bookmarking proof of concept built on a rainy Saturday
+      morning. Currently displaying {posts.length} {posts.length === 1 ? 'link' : 'links'}.
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+  await prisma.$connect()
+
+  const posts = await prisma.post
+    .findMany()
+    .then((response) => JSON.parse(JSON.stringify(response)))
+
+  await prisma.$disconnect()
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
 
 export default Page
